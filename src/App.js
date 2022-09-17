0aceca338e77e3bbcc2a55c4bb25608c487ebae9
Sim <zh025700@naver.com>
2022-09-17 16:58:18 +0900

    feat player(#01)

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import "./App.css";
 import Controller from "./components/Controller";
 import Player from "./components/Player";
 import SideBar from "./components/SideBar";
+import MediaPlayer from "./MediaPlayer";
 import GlobalStyle from "./style/GlobalStyle";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Controller />
-      <Player />
-      <SideBar />
+      <MediaPlayer />
     </>
   );
 }
