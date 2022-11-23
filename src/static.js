6675a405a52ccc3abe5552682cd58bf1a8a8956d
Sim <zh025700@naver.com>
2022-11-23 21:27:13 +0900

    fix: static url 변경

--- src/static.js
+++ src/static.js
@@ -1,4 +1,4 @@
-export const STATICURL = "http://34.64.177.193:80";
+export const STATICURL = "http://34.64.177.193/api";
 export const TESTTOKEN =
   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdHJpbmciLCJyb2xlcyI6WyJST0xFX1VTRVIiXSwiaWF0IjoxNjY4NDIwNTUyLCJleHAiOjE2OTk5NTY1NTJ9.-yNC5_VG7AarMyIZzHvzrh0hiDbcT08A6HhYr6UWcmQ";
 export const TESTCOURSE = 1;
